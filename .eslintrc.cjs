b9d9298bf457aba4768a5664e45c8d8bde16c6b8
OnyanPokon <mohamadrafiqd@gmail.com>
2024-01-14 12:53:59 +0800

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true, node: true},
+  extends: [
+    'airbnb',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  rules: {
+    'linebreak-style': 'off',
+    'import/no-extraneous-dependencies' : 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+  },
+}
